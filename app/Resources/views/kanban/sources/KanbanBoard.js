144b01141a6180c3759c24ea48c96e5070354b83
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-17 17:49:02 -0300

    Fixing typo

--- app/Resources/views/kanban/sources/KanbanBoard.js
+++ app/Resources/views/kanban/sources/KanbanBoard.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import List from './List';
 
-class KabanBoard extends Component 
+class KanbanBoard extends Component 
 {
     render() {
         return (
