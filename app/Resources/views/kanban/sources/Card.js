6055d5420eeacc33782d445737542a6f46a3cb20
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-20 18:58:21 -0300

    Adding propTypes validations

--- app/Resources/views/kanban/sources/Card.js
+++ app/Resources/views/kanban/sources/Card.js
@@ -7,7 +7,7 @@ import marked from 'marked';
 let titlePropType = (props, propName, componentName) => {
     if (props[propName]) {
         let value = props[propName];
-        if (typeOf value !== 'string' || value.length > 80) {
+        if (typeof value !== 'string' || value.length > 80) {
             return new Error(
                 `${propName} in ${componentName} is longer than 80 characters`
             );
