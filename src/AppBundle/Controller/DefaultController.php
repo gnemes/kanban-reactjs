5d414b876b0072cbc50bcc5e0a0fbd12d94a05be
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-24 19:21:48 -0300

    Deleting unused use statement

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -14,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        // KanbanBoard main page rendering
         return $this->render('kanban/index.html', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
