ea6868f9b9700eecec90b7bc3b505538447adb6a
root <root@localhost.localdomain>
2016-03-25 09:33:21 -0400

    Cards and tasks entities

---
+++ src/Api/ApiBundle/Entity/Tasks.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Api\ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tasks
+ *
+ * @ORM\Table(name="tasks")
+ * @ORM\Entity(repositoryClass="Api\ApiBundle\Repository\TasksRepository")
+ */
+class Tasks
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=30)
+     */
+    private $name;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="done", type="boolean")
+     */
+    private $done;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tasks
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set done
+     *
+     * @param boolean $done
+     *
+     * @return Tasks
+     */
+    public function setDone($done)
+    {
+        $this->done = $done;
+
+        return $this;
+    }
+
+    /**
+     * Get done
+     *
+     * @return bool
+     */
+    public function getDone()
+    {
+        return $this->done;
+    }
+}
+
